4ec9fc2fffba71b0fdce2b2007310879e3c29606
rafael simao <raficher@gmail.com>
2012-10-23 09:42:12 -0200

    Organizada a classe ShareListActivity, separando as funcionalidades dos botoes, deixando em aberto um espaco onde deve se pegar os dados que seram postados na midia social dependendo do botao clicado(pegar dados de TopArtists,TopSongs,ShareApp).

--- src/br/unb/mobileMedia/core/view/ShareListActivity.java
+++ src/br/unb/mobileMedia/core/view/ShareListActivity.java
@@ -6,38 +6,80 @@ import org.brickred.socialauth.android.SocialAuthAdapter.Provider;
 import org.brickred.socialauth.android.SocialAuthError;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Button;
 
 public class ShareListActivity extends Activity{
 
-	private SocialAuthAdapter adapter;
-	private Button btTopArtists;
+	private SocialAuthAdapter adapterTopArtists,adapterTopSongs,adapterShareApp;
+	private Button btnTopArtists,btnTopSongs,btnShareApp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(br.unb.mobileMedia.R.layout.activity_share_list);
 		
-		btTopArtists = (Button)findViewById(br.unb.mobileMedia.R.id.btn_top_artists);
-		btTopArtists.setText("Share");
+		btnTopArtists = (Button)findViewById(br.unb.mobileMedia.R.id.btn_top_artists);
+		btnTopSongs = (Button)findViewById(br.unb.mobileMedia.R.id.btn_top_songs);
+		btnShareApp = (Button)findViewById(br.unb.mobileMedia.R.id.btn_share_app);
+/*		btTopArtists.setText(br.unb.mobileMedia.R.string.btn_top_artists);
 		btTopArtists.setTextColor(Color.WHITE);
 		btTopArtists.setBackgroundResource(br.unb.mobileMedia.R.drawable.button_gradient);
 		
-		Log.d(getLocalClassName(), "ESTOI AKI");
-		ResponseListener listener = new ResponseListener();
-		adapter = new SocialAuthAdapter(listener);
-		Log.d(getLocalClassName(), "QUERENDO-TE");
+//		Log.d(getLocalClassName(), "ESTOI AKI");
+		ResponseListener listenerTopArtists = new ResponseListener("MM UnB Totalmente Funcional via App");
+		adapterTopArtists = new SocialAuthAdapter(listenerTopArtists);
+//		Log.d(getLocalClassName(), "QUERENDO-TE");
+		
+		adapterTopArtists.addProvider(Provider.FACEBOOK, br.unb.mobileMedia.R.drawable.facebook);
+		adapterTopArtists.addProvider(Provider.TWITTER, br.unb.mobileMedia.R.drawable.twitter);
+		
+		adapterTopArtists.enable(btnTopArtists);
+*/
+		ResponseListener listener = new ResponseListener("default",adapterTopArtists);
+		String conteudo = "DEFAULT";
+		//TopArtists
+		adapterTopArtists = new SocialAuthAdapter(listener);
+		/*
+		 * Pegar as informacoes dos Top Artistas e passar para a string conteudo
+		 * */
+		configAdapter(adapterTopArtists,conteudo,btnTopArtists);
+		
+		//TopSongs
+		adapterTopSongs = new SocialAuthAdapter(listener);
+		/*
+		 * Pegar as informacoes dos Top Songs e passar para a string conteudo
+		 * */
+		configAdapter(adapterTopSongs,conteudo,btnTopSongs);
+		
+		//ShareApp
+		adapterShareApp = new SocialAuthAdapter(listener);
+		/*
+		 * Pegar as informacoes dos ShareAPP e passar para a string conteudo
+		 * */
+		configAdapter(adapterShareApp,conteudo,btnShareApp);
+		
+	}
+	
+	private void configAdapter(SocialAuthAdapter adapter, String conteudo, Button btn){
+		ResponseListener listener = new ResponseListener(conteudo,adapter);
+		adapter.setListener(listener);
 		
 		adapter.addProvider(Provider.FACEBOOK, br.unb.mobileMedia.R.drawable.facebook);
 		adapter.addProvider(Provider.TWITTER, br.unb.mobileMedia.R.drawable.twitter);
 		
-		adapter.enable(btTopArtists);
+		adapter.enable(btn);
 	}
 	
 	private final class ResponseListener implements DialogListener {
+		
+		private SocialAuthAdapter adapter;
+		private String conteudo;
+		
+		public ResponseListener(String conteudo,SocialAuthAdapter adapter){
+			this.conteudo = conteudo;
+			this.adapter = adapter;
+		}
 
 		public void onCancel() {
 			// TODO Auto-generated method stub
@@ -45,7 +87,7 @@ public class ShareListActivity extends Activity{
 		}
 
 		public void onComplete(Bundle arg0) {
-			adapter.updateStatus("MM UnB Totalmente Funcional via App");
+			adapter.updateStatus(conteudo);
 		}
 
 		public void onError(SocialAuthError arg0) {
